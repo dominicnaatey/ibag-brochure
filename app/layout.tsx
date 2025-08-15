419c1d105e012ab1888cb06075936760853d5410
dominicnaatey <dominicnaatey@gmail.com>
2025-08-15 19:56:37 +0000

    style(layout): update font variable from inter to dmSans

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={inter.variable} suppressHydrationWarning>
+    <html lang="en" className={dmSans.variable} suppressHydrationWarning>
       <head>
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
