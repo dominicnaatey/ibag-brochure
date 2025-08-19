2ef21580824220e88fa0d3c12b46399c1ceba534
dominicnaatey <dominicnaatey@gmail.com>
2025-08-19 14:23:24 +0000

    perf(image-carousel): reduce image transition duration for smoother UX

--- components/image-carousel.tsx
+++ components/image-carousel.tsx
@@ -32,7 +32,7 @@ export function ImageCarousel({ images, autoPlay = true, interval = 10000 }: Ima
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ 
-            duration: 1.2,
+            duration: 0.8,
             ease: "easeInOut"
           }}
           className="absolute inset-0 w-full h-full"
