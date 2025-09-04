710331fa76ca3e9d3f59fe69447ee66b392e70c4
dominicnaatey <dominicnaatey@gmail.com>
2025-09-04 00:06:11 +0000

    disable PWA in development environment

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,7 @@ const withPWAConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
   runtimeCaching: [
     {
       urlPattern: /^https:\/\/fonts\.(gstatic|googleapis)\.com\/.*/i,
